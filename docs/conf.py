e620c82fa0fe817122962fce5c6bd42d82c49563
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-06-17 12:02:27 +0200

    Bump version in config file

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, @digisus, @benjaminaaron'
 author = '@digisus, @benjaminaaron'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.0'
+release = '0.7.0'
 
 
 # -- General configuration ---------------------------------------------------
